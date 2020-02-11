04eef6cf2e52cb9fdf8f7318fbbc736fbb00a335
junior1994 <junior_duff_sm@hotmail.com>
2020-02-10 22:38:37 -0300

    Finalizando API de cadastro e update

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -16,8 +16,10 @@ export default async(req, res, next) => {
 
     try {
         const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-        console.log(decoded)
+        req.userId = decoded.id;
+
         return next();
+
 
     } catch (error) {
         return res.status(401).json({ error: 'token invalid' })
